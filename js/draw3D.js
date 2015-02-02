8a92fd5771257e6df56fe6d69ec0b108446dbb1f
Gustav Rannestig <gustav.rannestig@prototyp.se>
2015-02-02 11:19:54 +0100

    refactor to use js instead of ps

--- js/draw3D.js
+++ js/draw3D.js
@@ -14,25 +14,25 @@ var gridRect;
 
 var init = function() {
 	//The main path
-	myPath = new Path();
+	myPath = new paper.Path();
 	myPath.strokeColor = 'black';
 
 	//init render button
-	var buttonPosition = new Point(10,10);
-	var buttonSize = new Size(10, 10);
-	var button = new Path.Rectangle(buttonPosition, buttonSize);
+	var buttonPosition = new paper.Point(10,10);
+	var buttonSize = new paper.Size(10, 10);
+	var button = new paper.Path.Rectangle(buttonPosition, buttonSize);
 	button.strokeColor = 'black';
-	buttonRect = new Rectangle(buttonPosition, buttonSize);
+	buttonRect = new paper.Rectangle(buttonPosition, buttonSize);
 
 	//init simple grid
-	center = new Point(300,300);
-	yAxisCenter = new Point(300,200);
-	yAxisTop = new Point(300, 100);
-	xAxisCenter = new Point(400, 300);
-	xAxisRight = new Point(500, 300);
+	center = new paper.Point(300,300);
+	yAxisCenter = new paper.Point(300,200);
+	yAxisTop = new paper.Point(300, 100);
+	xAxisCenter = new paper.Point(400, 300);
+	xAxisRight = new paper.Point(500, 300);
 
-	var yAxis = new Path();
-	var xAxis = new Path();
+	var yAxis = new paper.Path();
+	var xAxis = new paper.Path();
 
 	yAxis.add(yAxisTop);
 	yAxis.add(center);
@@ -43,54 +43,63 @@ var init = function() {
 	yAxis.strokeColor = 'black';
 	xAxis.strokeColor = 'black';
 
-	gridRect = new Rectangle(yAxisTop, new Size(200, 200));
+	gridRect = new paper.Rectangle(yAxisTop, new paper.Size(200, 200));
 
 	//add starting point to myPath
 	myPath.add(yAxisCenter);
 
 	//Add mirroring paths
-	firstPath = new Path();
-	thirdPath = new Path();
-	fourthPath = new Path();
+	firstPath = new paper.Path();
+	thirdPath = new paper.Path();
+	fourthPath = new paper.Path();
 
 	firstPath.add(yAxisCenter);
-	thirdPath.add(new Point(300,400));
-	fourthPath.add(new Point(300, 400));
+	thirdPath.add(new paper.Point(300,400));
+	fourthPath.add(new paper.Point(300, 400));
 
 	firstPath.strokeColor = 'black';
 	thirdPath.strokeColor = 'black';
 	fourthPath.strokeColor = 'black';
+	paper.view.draw();	
 }
 
 var mirrorPath = function(point) {
 	//mirror the point in the first, third and fourth qadrant
-	var firstPoint = new Point();
+	var firstPoint = new paper.Point();
 	firstPoint.x = yAxisCenter.x - (point.x - yAxisCenter.x);
 	firstPoint.y = xAxisCenter.y + (point.y - xAxisCenter.y);	
 	firstPath.add(firstPoint);
 
-	var thirdPoint = new Point();
+	var thirdPoint = new paper.Point();
 	thirdPoint.x = yAxisCenter.x - (point.x - yAxisCenter.x);
 	thirdPoint.y = xAxisCenter.y  + (xAxisCenter.y - point.y);
 	thirdPath.add(thirdPoint);
 
-	var fourthPoint = new Point();
+	var fourthPoint = new paper.Point();
 	fourthPoint.x = point.x;
 	fourthPoint.y = thirdPoint.y;
 	fourthPath.add(fourthPoint);
 }
 
-function onMouseDown(event) {
-	// Add a segment to the path at the position of the mouse:
-	if(event.point.isInside(buttonRect)){
-		console.log("button pressed");
-		myPath.add(xAxisCenter);
-		mirrorPath(xAxisCenter);
-	}
-	else if(event.point.isInside(gridRect)) {
-		myPath.add(event.point);
-		mirrorPath(event.point);
+
+window.onload = function() {
+	paper.setup('paperCanvas');
+	
+	init();
+
+	var tool = new paper.Tool();
+
+	tool.onMouseDown = function onMouseDown(event) {
+		// Add a segment to the path at the position of the mouse:
+		if(event.point.isInside(buttonRect)){
+			console.log("button pressed");
+			myPath.add(xAxisCenter);
+			mirrorPath(xAxisCenter);
+		}
+		else if(event.point.isInside(gridRect)) {
+			myPath.add(event.point);
+			mirrorPath(event.point);
+		}
+		paper.view.draw();
 	}
 }
-
-init();
